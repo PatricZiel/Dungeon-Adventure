73fdba114fb5f6712670c7122b23baae92cc502c
Patric Ziel <ZielPatric@GMail.com>
2022-10-21 20:48:37 +0200

    Start des Abenteuers ausgelagert

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static Hero hero = new Hero();
     public static void main(String[] args) {
         try {
-            Hero hero = new Hero();
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Seid gegrüst Reisender");
-            while (true) {
-                System.out.println("Wie ist euer Name?");
-                hero.name = br.readLine();
 
-                System.out.println("Euer Name ist also " + hero.name + "?");
-                System.out.println("1. Ja");
-                System.out.println("2. Name neu eingeben");
-
-                String decision = br.readLine();
-                if(decision.equals("1")) {
-                    break;
-                }
-            }
+            startAdventure();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void startAdventure() throws IOException {
+        while (true) {
+            System.out.println("Wie ist euer Name?");
+            hero.name = br.readLine();
+
+            System.out.println("Euer Name ist also " + hero.name + "?");
+            System.out.println("1. Ja");
+            System.out.println("2. Name neu eingeben");
+
+            String decision = br.readLine();
+            if(decision.equals("1")) {
+                break;
+            }
         }
     }
 }
