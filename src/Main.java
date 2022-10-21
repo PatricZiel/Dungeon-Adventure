56b5221975e1b4e35c003915218a8ab41f6ea461
Patric Ziel <ZielPatric@GMail.com>
2022-10-21 20:37:09 +0200

    Ich bin Patric

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import main.java.logic.entity.Hero;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Ich bin Patric");
+        try {
+            Hero hero = new Hero();
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Seid gegrüst Reisender");
+            while (true) {
+                System.out.println("Wie ist euer Name?");
+                hero.name = br.readLine();
+
+                System.out.println("Euer Name ist also " + hero.name + "?");
+                System.out.println("1. Ja");
+                System.out.println("2. Name neu eingeben");
+
+                String decision = br.readLine();
+                if(decision.equals("1")) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/logic/entity/Hero.java
@@ -0,0 +1,9 @@
+package main.java.logic.entity;
+
+public class Hero {
+    public String name = "Patric";
+
+    public Hero() {
+
+    }
+}
